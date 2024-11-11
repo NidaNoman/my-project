40f3f29e00e43c53d90b380c8081c42eb721e604
NidaNoman <mrsajjad3455@gmail.com>
2024-11-11 08:06:21 +0500

    files added

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import About from "./About/page"
+import Hero from "./hero"
+import Skills from "./skills/page"
+import Contact from "./contact/page"
+export default function Home(){
+  return(
+    <div>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Contact/>
+    
+      
+      
+    </div>
+  )
+}
